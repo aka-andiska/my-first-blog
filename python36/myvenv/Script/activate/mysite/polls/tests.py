383c2cbc6cff8170b0a1df74976e2fae972bb208
aka <aka@junjies-MacBook-Pro.local>
2016-12-22 16:45:43 +0800

    more comprehensive test 'was_published_recently()'

--- python36/myvenv/Script/activate/mysite/polls/tests.py
+++ python36/myvenv/Script/activate/mysite/polls/tests.py
@@ -16,3 +16,12 @@ class QuestionMethodTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() should return True for questions whose
+        pub_date is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
