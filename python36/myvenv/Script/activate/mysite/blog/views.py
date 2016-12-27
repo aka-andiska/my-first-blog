db2f370cd6c7f5539ff9de8ffe8711b82719d997
aka <aka@junjies-MacBook-Pro.local>
2016-12-27 17:50:12 +0800

    dinamic object and queryset

--- python36/myvenv/Script/activate/mysite/blog/views.py
+++ python36/myvenv/Script/activate/mysite/blog/views.py
@@ -4,8 +4,6 @@ from .models import Post
 
 # Create your views here.
 
-Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {})
