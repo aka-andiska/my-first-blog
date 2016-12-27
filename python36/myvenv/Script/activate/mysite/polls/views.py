71fb55247bca99916c75fa08e025bb157ebe9a1f
aka <aka@junjies-MacBook-Pro.local>
2016-12-27 10:43:21 +0800

    style.css

--- python36/myvenv/Script/activate/mysite/polls/views.py
+++ python36/myvenv/Script/activate/mysite/polls/views.py
@@ -56,9 +56,7 @@ def index(request):
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
+    context = {'latest_question_list': latest_question_list,}
     return HttpResponse(template.render(context, request))
 
 
